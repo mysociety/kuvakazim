076e96ed5c3c383357175f07ddafb41a3507cb70
Chris Mytton <chrismytton@gmail.com>
2015-11-25 13:43:32 +0000

    Add jekyll-popolo plugin

---
+++ _plugins/popolo.rb
@@ -0,0 +1,34 @@
+require 'json'
+require 'open-uri'
+
+require 'active_support'
+require 'active_support/core_ext/string'
+
+module Jekyll
+  class PopoloDocument < Document
+  end
+
+  class Popolo < Generator
+    def generate(site)
+      popolo = JSON.parse(open(site.config['popolo_url']).read)
+      popolo.keys.each do |collection_name|
+        next unless popolo[collection_name].is_a?(Array)
+        collection = Collection.new(site, collection_name)
+        popolo[collection_name].each do |item|
+          next unless item['id']
+          path = File.join(site.config['source'], "_#{collection_name}", "#{item['id'].parameterize}.md")
+          doc = PopoloDocument.new(path, collection: collection, site: site)
+          doc.merge_data!(item)
+          doc.merge_data!(
+            'title' => item['name'],
+            'layout' => collection_name
+          )
+          collection.docs << doc
+        end
+        site.collections[collection_name] = collection
+      end
+
+      site.data['memberships'] = popolo['memberships']
+    end
+  end
+end
