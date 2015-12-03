6df69ee636f45713f6031d7df5d328d038a5f141
Chris Mytton <chrism@mysociety.org>
2015-12-03 14:43:47 +0000

    Add senate

--- _plugins/everypolitician_datasource.rb
+++ _plugins/everypolitician_datasource.rb
@@ -9,7 +9,9 @@ Jekyll::Hooks.register :site, :pre_render do |site, payload|
   sprockets = site.sprockets
   asset = sprockets.find_asset('person-210x210.jpg')
   asset_path = sprockets.prefix_path(sprockets.digest? ? asset.digest_path : asset.logical_path)
-  site.collections['people'].docs.each do |person|
-    person.data['image'] ||= asset_path
+  %w[assembly_people senate_people].each do |collection|
+    site.collections[collection].docs.each do |person|
+      person.data['image'] ||= asset_path
+    end
   end
 end
