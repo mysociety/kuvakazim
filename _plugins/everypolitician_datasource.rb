0d6cd4d4fe3c17fc248b76833ec9539701890482
Chris Mytton <chrism@mysociety.org>
2015-12-03 16:36:05 +0000

    Make education work with remote_csv plugin

--- _plugins/everypolitician_datasource.rb
+++ _plugins/everypolitician_datasource.rb
@@ -5,13 +5,25 @@ Jekyll::Hooks.register :site, :post_read do |site|
   }
 end
 
-Jekyll::Hooks.register :site, :pre_render do |site, payload|
-  sprockets = site.sprockets
-  asset = sprockets.find_asset('person-210x210.jpg')
-  asset_path = sprockets.prefix_path(sprockets.digest? ? asset.digest_path : asset.logical_path)
-  %w[assembly_people senate_people].each do |collection|
-    site.collections[collection].docs.each do |person|
-      person.data['image'] ||= asset_path
+module Jekyll
+  class DataCleanup < Jekyll::Generator
+    def generate(site)
+      %w[assembly_people senate_people].each do |collection|
+        site.collections[collection].docs.each do |person|
+          person.data['image'] ||= asset_path(site)
+          kuvakazim_identifer = person.data['identifiers'].find { |id| id['scheme'] == 'kuvakazim' }
+          next unless kuvakazim_identifer
+          person.data['kuvakazim_id'] = kuvakazim_identifer['identifier']
+        end
+      end
+    end
+
+    def asset_path(site)
+      @asset_path ||= begin
+        sprockets = site.sprockets
+        asset = sprockets.find_asset('person-210x210.jpg')
+        sprockets.prefix_path(sprockets.digest? ? asset.digest_path : asset.logical_path)
+      end
     end
   end
 end
