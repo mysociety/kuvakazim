aba73f31b58b17df6a5fc1fa142689089ccd46ed
everypoliticianbot <team@everypolitician.org>
2015-12-07 09:59:27 +0000

    Deploy to GitHub Pages

---
+++ javascripts/main.js
@@ -0,0 +1,23 @@
+/* Javascript for the official Twitter widget */
+
+jQuery(function($) {
+    !function (d, s, id) {
+        var js,
+        fjs = d.getElementsByTagName(s)[0],
+        p = /^http:/.test(d.location) ? 'http' : 'https';
+        if (!d.getElementById(id)) {
+            js = d.createElement(s);
+            js.id = id;
+            js.src = p + "://platform.twitter.com/widgets.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }
+    }(document, "script", "twitter-wjs");
+});
+
+jQuery(function($) {
+  var $searchBox = $('.js-search-autocomplete-name')
+  $searchBox.select2();
+  $searchBox.change(function(e) {
+    window.location.href = $(this).val();
+  });
+});
