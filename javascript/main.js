c6359c467cec58c6b46aabad72796d53b914d858
everypoliticianbot <team@everypolitician.org>
2015-12-11 17:20:31 +0000

    Deploy to GitHub Pages

--- javascript/main.js
+++
@@ -1,7 +0,0 @@
-jQuery(function($) {
-  var $searchBox = $('.js-search-autocomplete-name')
-  $searchBox.select2();
-  $searchBox.change(function(e) {
-    window.location.href = $(this).val();
-  });
-});
