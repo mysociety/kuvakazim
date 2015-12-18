ecc8d2d112c136ec900f1387af608ef0523804b9
Chris Mytton <chrism@mysociety.org>
2015-12-18 15:29:29 +0000

    Redirect legacy organization and area urls

--- _plugins/redirects.rb
+++ _plugins/redirects.rb
@@ -13,5 +13,27 @@ class RedirectGenerator < Jekyll::Generator
         ]
       end
     end
+
+    %w(assembly_organizations senate_organizations).each do |collection|
+      site.collections[collection].docs.each do |doc|
+        slug = Jekyll::Utils.slugify(doc.data['id'].split('/').last)
+        doc.data['redirect_from'] = [
+          "/organisation/#{slug}/"
+        ]
+      end
+    end
+
+    %w(assembly_areas senate_areas).each do |collection|
+      site.collections[collection].docs.each do |doc|
+        slug = Jekyll::Utils.slugify(doc.data['id'].split('/').last)
+        # Backward compatibility for this one case where slugs differ
+        if slug == 'st-mary-s'
+          slug = 'st-marys'
+        end
+        doc.data['redirect_from'] = [
+          "/place/#{slug}/"
+        ]
+      end
+    end
   end
 end
