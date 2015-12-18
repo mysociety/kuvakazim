4a6f24b5d740801fa9c76f40e07ad5e4d576c23a
Chris Mytton <chrism@mysociety.org>
2015-12-18 18:25:10 +0000

    Don't treat person images specially
    Rather than populating these in a plugin it's clearer to just use a
    conditional in the template so designers can easily override the default
    image if necessasry.

--- _plugins/everypolitician_datasource.rb
+++ _plugins/everypolitician_datasource.rb
@@ -19,7 +19,6 @@ module Jekyll
     def generate(site)
       %w[assembly_people senate_people].each do |collection|
         site.collections[collection].docs.each do |person|
-          person.data['image'] ||= '/images/person-210x210.jpg'
           kuvakazim_identifer = person.data['identifiers'].find { |id| id['scheme'] == 'kuvakazim' }
           next unless kuvakazim_identifer
           person.data['kuvakazim_id'] = kuvakazim_identifer['identifier']
