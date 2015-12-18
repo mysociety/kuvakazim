c76e7ed328601dd0c30c052b8526fb4f7685fb58
Chris Mytton <chrism@mysociety.org>
2015-12-18 14:58:17 +0000

    Redirect senate people as well as assembly

--- _plugins/redirects.rb
+++ _plugins/redirects.rb
@@ -4,12 +4,14 @@ class RedirectGenerator < Jekyll::Generator
 
   def generate(site)
     slug_mapping = site.data['people_slugs']
-    site.collections['assembly_people'].docs.each do |doc|
-      kuvakazim_identifier = doc.data['identifiers'].find { |id| id['scheme'] == 'kuvakazim' }
-      next unless kuvakazim_identifier
-      doc.data['redirect_from'] = [
-        "/person/#{slug_mapping[kuvakazim_identifier['identifier'].to_s]}/"
-      ]
+    %w(assembly_people senate_people).each do |collection|
+      site.collections[collection].docs.each do |doc|
+        kuvakazim_identifier = doc.data['identifiers'].find { |id| id['scheme'] == 'kuvakazim' }
+        next unless kuvakazim_identifier
+        doc.data['redirect_from'] = [
+          "/person/#{slug_mapping[kuvakazim_identifier['identifier'].to_s]}/"
+        ]
+      end
     end
   end
 end
